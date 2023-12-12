74ca9380a0eead8bac1077ebdedfa168c2fe359b
Clay Boone <tener.hades@gmail.com>
2023-12-12 00:18:17 +0000

    🎼 down in a hole 🎵 mmm hmmm 🎶 losing my soul 🔥

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // use challenges::day01::day1_part1;
-use challenges::day01::day1_part2;
+// use challenges::day01::day1_part2;
 use std::{io::BufRead, time::Instant};
+
+use std::sync::mpsc;
+use std::thread;
 
 mod challenges {
     pub mod day01 {
@@ -39,38 +42,68 @@ fn main() {
             // day1_part2 as fn(&String) -> i32,
             0,
         ),
-        // (
-        //     "Day 1, Part 2, 1000mb input",
-        //     String::from("src/challenges/day01/big_input_1000mb.txt"),
-        //     day1_part2 as fn(&String) -> i32,
-        //     0,
-        // ),
-        // (
-        //     "Day 1, Part 2, 10000mb input",
-        //     String::from("src/challenges/day01/big_input_10000mb.txt"),
-        //     day1_part2 as fn(&String) -> i32,
-        //     0,
-        // ),
+        (
+            "Day 1, Part 2, 1000mb input",
+            String::from("src/challenges/day01/big_input_1000mb.txt"),
+            // day1_part2 as fn(&String) -> i32,
+            0,
+        ),
+        (
+            "Day 1, Part 2, 10000mb input",
+            String::from("src/challenges/day01/big_input_10000mb.txt"),
+            // day1_part2 as fn(&String) -> i32,
+            0,
+        ),
     ] {
-        // let input = std::fs::read_to_string(input).unwrap();
-        // read line-by-line
-        let file = std::fs::File::open(input).unwrap();
-        let reader = std::io::BufReader::new(file);
-        // let mut input = String::new();
-        let mut result = 0;
+        let (tx, rx) = mpsc::channel();
+
+        // Read line-by-line in a thread.
+        let reader_thread = thread::spawn(move || {
+            if let Ok(file) = std::fs::File::open(&input) {
+                let reader = std::io::BufReader::new(file);
+                for line in reader.lines() {
+                    tx.send(line.unwrap()).unwrap();
+                }
+            }
+            drop(tx);
+        });
+
+        // Process lines in another thread.
+        let accumulator_thread = thread::spawn(move || {
+            let mut result = 0;
+            for line in rx {
+                result += get_first_and_last_digits(&line)
+                    .parse::<i32>()
+                    .unwrap();
+            }
+            result
+        });
 
         start_time = Instant::now();
-        for line in reader.lines() {
-            // input.push_str(&line.unwrap());
-            // reduce here to avoid allocating a new string for every line
-            // TODO
-            result += get_first_and_last_digits(&line.unwrap())
-                .parse::<i32>()
-                .unwrap();
-        }
+        reader_thread.join().unwrap();
+        let result = accumulator_thread.join().unwrap();
+        println!("{}: {}ms", day_and_part, start_time.elapsed().as_millis());
+
+
+        // // let input = std::fs::read_to_string(input).unwrap();
+        // // read line-by-line
+        // let file = std::fs::File::open(input).unwrap();
+        // let reader = std::io::BufReader::new(file);
+        // // let mut input = String::new();
+        // let mut result = 0;
+
+        // start_time = Instant::now();
+        // for line in reader.lines() {
+        //     // input.push_str(&line.unwrap());
+        //     // reduce here to avoid allocating a new string for every line
+        //     // TODO
+        //     result += get_first_and_last_digits(&line.unwrap())
+        //         .parse::<i32>()
+        //         .unwrap();
+        // }
 
         // let result = func(&input);
-        println!("{}: {}ms", day_and_part, start_time.elapsed().as_millis());
+        // println!("{}: {}ms", day_and_part, start_time.elapsed().as_millis());
 
         println!(
             "{}: {} ({})",
